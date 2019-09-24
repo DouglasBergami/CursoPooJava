3d0f0a40735345ec34718f91900acb1538c8f2f0
douglas.bergami <douglas.t.k.d@hotmail.com>
2019-09-24 00:55:45 -0300

    utilização de pipeline

--- src/ProgramGeneric/Program.java
+++ src/ProgramGeneric/Program.java
@@ -355,4 +355,21 @@ public class Program {
         System.out.println(Arrays.toString(st4.limit(20).toArray()));
         
     }
+    
+    public static void Executepipeline(){
+        
+         List<Integer> list = Arrays.asList(3,4,5,10,7);
+         
+         Stream<Integer> st1 = list.stream().map(x -> x * 10);
+         System.out.println(Arrays.toString(st1.toArray()));
+         
+         int soma = list.stream().reduce(0, (x ,y) -> x + y);
+         System.out.println("soma: " + soma);
+         
+         List<Integer> newList = list.stream()
+                 .filter(x -> x % 2 ==0)
+                 .map(x -> x * 10)
+                 .collect(Collectors.toList());
+         System.out.println(Arrays.toString(newList.toArray()));
+    }
 }
--- src/application/Application.java
+++ src/application/Application.java
@@ -4,7 +4,7 @@ public class Application {
    
     public static void main(String[] args) {
         
-        ProgramGeneric.Program.executeStream();
+        ProgramGeneric.Program.Executepipeline();
      
     }
     
