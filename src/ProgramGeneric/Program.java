7f0f4325f6ef5855b61be32234e31a4b967bb108
douglas.bergami <douglas.t.k.d@hotmail.com>
2019-09-24 00:31:54 -0300

    utilização de stream e exemplos

--- src/ProgramGeneric/Program.java
+++ src/ProgramGeneric/Program.java
@@ -25,6 +25,7 @@ import java.util.Locale;
 import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Program {
 
@@ -333,6 +334,25 @@ public class Program {
         double sum = ps.filteredSum(listProduct, p -> p.getName().charAt(0) == 'T');
         
         System.out.println(sum);
+        
+    }
+    
+    public static void executeStream(){
+        
+        //Demonstração de uso com stream
+        
+        List<Integer> list = Arrays.asList(1,2,3,4,5);
+        Stream<Integer> st1 = list.stream();
+        System.out.println(Arrays.toString(st1.toArray()));
+        
+        Stream<String> st2 = Stream.of("Maria", "Alex", "Bob");
+        System.out.println(Arrays.toString(st2.toArray()));
+        
+        Stream<Integer> st3 = Stream.iterate(0, x -> x +2);
+        System.out.println(Arrays.toString(st3.limit(10).toArray()));
+        
+        Stream<Long> st4 = Stream.iterate(new Long[] {0L, 1L}, p -> new Long[] {p[1], p[0]+p[1]}).map(p -> p[0]);
+        System.out.println(Arrays.toString(st4.limit(20).toArray()));
         
     }
 }
